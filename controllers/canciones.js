211f2edc5987aebb402d538cd2b912b642e0d4c7
ublan <uri.blan57@gmail.com>
2024-09-12 15:40:21 -0300

    arreglo de filtrado

--- controllers/canciones.js
+++ controllers/canciones.js
@@ -2,27 +2,44 @@ const axios = require('axios')
 const { request, response } = require('express')
 
 const getCanciones = (req = request, res = response) => {
-  const { nombre = '', nombre_artista = '', genero = '', reproducciones = '' } = req.query
-  console.log(nombre, nombre_artista, genero, reproducciones)
+  const { nombre = '', nombre_artista = '', genero = '', reproducciones = '' } = req.query;
+  console.log(nombre, nombre_artista, genero, reproducciones);
 
-  const filtro = (nombre) ? `?nombre=${nombre}&reproducciones=${reproducciones}` : ''
+  let filtro = ''; // Variable para ver si hay filtro
+  
+  if (nombre) {
+    filtro += `?nombre=${nombre}`;
+  }
+  
+  if (nombre_artista) {
+    filtro += filtro ? `&nombre_artista=${nombre_artista}` : `?nombre_artista=${nombre_artista}`;
+  }
+  
+  if (genero) {
+    filtro += filtro ? `&genero=${genero}` : `?genero=${genero}`;
+  }
+  
+  if (reproducciones) {
+    filtro += filtro ? `&reproducciones=${reproducciones}` : `?reproducciones=${reproducciones}`;
+  }
 
   axios.get(`https://66dc500547d749b72acb464f.mockapi.io/api/canciones${filtro}`)
     .then((response) => {
-      const { data = [] } = response   // handle success
+      const { data = [] } = response;  // manejar éxito
       res.status(200).json({
         msg: 'Ok',
         data
-      })
+      });
     })
-    .catch((error) => {     // handle error
-      console.log(error)
+    .catch((error) => {  // manejar error
+      console.log(error);
       res.status(400).json({
         msg: 'Error',
         error
-      })
-    })
-}
+      });
+    });
+};
+
 
 const getCancion = (req = request, res = response) => {
   const { id = '' } = req.params
@@ -49,5 +66,4 @@ const getCancion = (req = request, res = response) => {
 module.exports = {
   getCanciones,
   getCancion,
-  getPopulares
 }
